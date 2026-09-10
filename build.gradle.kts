ca77f0782127b2dfbe67dd7e5d0ea0a190dc7572
beikesir <2235927716@qq.com>
2026-09-10 15:15:26 +0800

    删除

--- build.gradle.kts
+++
@@ -1,7 +0,0 @@
-// 根构建脚本：只声明插件，不应用（apply false）
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-}
